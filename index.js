6a831adf7e876f06bddbd16e16e0e9d1a4f90dcf
Felipe Corredor <pipecorredor06@gmail.com>
2021-04-09 12:40:57 -0500

    Upload server graphql

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const { readFileSync } = require('fs')
 const { join } = require('path')
 const express = require('express')
 const resolvers = require('./lib/resolvers')
+const cors = require('cors')
 
 // initialize express
 const app = express()
 const port = process.env.port || 5000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definiendo el schema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
@@ -16,13 +18,14 @@ const schema = makeExecutableSchema({
   resolvers,
 })
 
+app.use(cors())
 // execute query
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: isDev,
   })
 )
 
